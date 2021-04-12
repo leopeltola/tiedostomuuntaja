5573698fb8f58317f10866254024d464a3d3c553
Leo P <leopeltola@gmail.com>
2021-04-13 00:16:19 +0300

    Doesn't save anything to file now, just keeps in memory

--- convert.py
+++ convert.py
@@ -4,9 +4,10 @@ from io import BytesIO
 def convert_to_pdf(img):
 	f = BytesIO()
 	img = img.convert("RGB")
-	# img.save(f, format="png")
-	img.save("files/temp.pdf")
-	# f.seek(1)
+	img = img.rotate(-90)
+	img.save(f, format="pdf")
+	# img.save("files/temp.pdf")
+	f.seek(1)
 	# img = Image.open(f)
-	# return img
-	return "files/temp.pdf"
+	return f
+	# return "files/temp.pdf"
--- server.py
+++ server.py
@@ -4,27 +4,38 @@ from convert import convert_to_pdf
 from PIL import Image
 from io import BytesIO
 from flask_wtf import FlaskForm
-from wtforms import FileField
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 from os import remove
 
-class FileUploadForm(FlaskForm):
-	file = FileField()
+class UploadForm(FlaskForm):
+	file = FileField("file", validators=[DataRequired()])
+	submit = SubmitField("submit")
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "super-secret-key"
+app.config["SECRET_KEY"] = "not-so-secret-key"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	form = FileUploadForm()
-	if request.method == "POST":
+	form = UploadForm()
+	if form.validate_on_submit():
+		if 'file' not in request.files:
+			flash('Tiedostoa ei löydetty')
+			return render_template("main.html", form=form)
 		file = request.files["file"]
-		file.save("files/{}".format(file.filename))
-		img = Image.open("files/{}".format(file.filename))
-		pdf_path = convert_to_pdf(img)
+		if file.filename == '':
+			flash('Ei valittua tiedostoa')
+			return render_template("main.html", form=form)
+		f = BytesIO()
+		file.save(f)
+		img = Image.open(f)
+		pdf = convert_to_pdf(img)
 		img.close()
-		remove("files/{}".format(file.filename))
-		return send_file(pdf_path, attachment_filename='converted_file.pdf', as_attachment=True)
+		pdf.seek(0)
+		i = file.filename.find(".")
+		name = file.filename[:i]
+		return send_file(pdf, attachment_filename='{}.pdf'.format(name), as_attachment=True)
 
 		# return render_template("main.html")
-	return render_template("main.html")
+	return render_template("main.html", form=form)
